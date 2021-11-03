083d66d7d75dc00b89d4b956cc01f7b70f22017b
robwhite <robert.white3120@gmail.com>
2021-11-03 16:41:58 -0500

    created basic initial 'home' design for the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Header from './components/header.js'
+import './App.css'
+import addSvg from './iconmonstr-plus-circle-thin.svg'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className="addSvg-container">
+        <img src={addSvg} className="add-svg" alt="add" />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
