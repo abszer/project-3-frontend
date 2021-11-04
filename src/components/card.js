fb3928cfff827dfd076f860295e5d6bcbc5032c3
Yianni Catege <ycatege@gmail.com>
2021-11-04 15:20:17 -0400

    added a delete button that removes the corresponding card

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import axios from 'axios'
 
-const Card = ({ card }) => {
+const Card = ({ card,setCardsData }) => {
+     const handleDelete=()=>{
+          axios.delete(`https://squadupgames.herokuapp.com/games/${card._id}`).then((response)=>{
+               axios.get("https://squadupgames.herokuapp.com/games").then((response) => {
+                    setCardsData(response.data)
+               })
+          })
+     }
      return(
           <div className="card">
                <h4>{card.name}</h4>
                <img src={card.image} />
                <h3>{card.description}</h3>
+               <button onClick={handleDelete}>Close Request</button>
           </div>
      )
 }
